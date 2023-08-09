aa09b57c3ceb2655f5e91959a3a6503cf6a80dd0
Melania Valdez <melanavaldez2803@gmail.com>
2023-08-09 20:35:57 -0300

    a

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,15 @@
+
+
+const CartWidget = () => {
+    
+  return (
+      <div className="CartWidget">
+          <a href="#">
+              <img src={carrito} alt='carrito'/>
+          </a>
+          <p>1</p>
+      </div>    
+  );
+}
+
+export default CartWidget;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return(
+        <nav>
+            <h3>Cafe</h3>
+            <div>
+                <button>opcion</button>
+                <button>clickeable</button>
+                <button>otra mas</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
